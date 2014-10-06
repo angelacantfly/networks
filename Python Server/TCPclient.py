aaeae3625656186666ca0d2db2b7a0ab4b47499d
Bruce Yan <byan@hmc.edu>
2014-10-05 20:04:35 -0700

    updated one of the files

--- Python Server/TCPclient.py
+++ Python Server/TCPclient.py
@@ -1,7 +1,7 @@
 # TCP client example
 import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("localhost", 5005))
+client_socket.connect(("nodea", 5005))
 while 1:
     data = client_socket.recv(512)
     if ( data == 'q' or data == 'Q'):
@@ -16,4 +16,4 @@ while 1:
             client_socket.send(data)
             client_socket.close()
             break;
-            
+
