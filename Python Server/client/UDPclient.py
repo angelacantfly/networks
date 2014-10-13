ae0778338fc281d9d11782167b12d285a2b596e9
Bruce Yan <byan@hmc.edu>
2014-10-12 23:26:32 -0700

    udp stuff

--- Python Server/client/UDPclient.py
+++ Python Server/client/UDPclient.py
@@ -5,6 +5,9 @@
 # UDP client example
 import socket
 import sys
+import os
+import convertIndex
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # client_socket.connect(("localhost", 5006))
 # Comment 1: We don't want the above line because unlike TCP, which needs to maintain a solid connection, we don't need to do that with UDP.  UDP is meant to be used as opening a connection and sending stuff over, then closing the connection when done.  Don't need to keep it open
@@ -12,8 +15,11 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 host = sys.argv[1]
 port = 5006
-buf = 512
 address = (host,port)
+# buf = 512
+BLOCK_SIZE = 512
+INDEX_SIZE = 8
+buf = BLOCK_SIZE - INDEX_SIZE
 
 filename = sys.argv[2]
 
@@ -22,15 +28,28 @@ client_socket.sendto(filename, address)
 # Open a file with the name of the transmitted file to read
 f = open(filename, "rb") # read binary
 
-# Read the data to buffer
+# Read an initial amount data to buffer
 data = f.read(buf)
+print data
+# index calculations - this is tacked to the front of data
+size = os.path.getsize(filename)
+numBlocks = size / BLOCK_SIZE
+if size % BLOCK_SIZE != 0:
+    numBlocks += 1
 
 # TODO: Attach a number to each
 # instead of sending 512, we send 408 so 4 bytes will be the index #
+index = 0
+
 while(data):
-    if(client_socket.sendto(data,address)):
-        print "Sending:",filename, "..."
+    index += 1
+    if(client_socket.sendto(convertIndex.convertIndexToStr(index, INDEX_SIZE) + data,address)):
+        print "Sending:",filename, "... index:", convertIndex.convertIndexToStr(index, INDEX_SIZE)
         data = f.read(buf)
+
+
+print "index is:",index
+print "total number of blocks is:", numBlocks
 
 client_socket.close()
 f.close()
--- Python Server/server/UDPserver.py
+++ Python Server/server/UDPserver.py
@@ -5,11 +5,16 @@
 # UDP server example
 # lsof -i udp:5006 // use this to test if port 5006 is free
 import socket
+import sys
+import os
 
 # Variables
 host = "0.0.0.0"
 port = 5006
-buf = 512 # adjustable, try higher
+# buf = 512 # adjustable, try higher
+BLOCK_SIZE = 512
+INDEX_SIZE = 8
+buf = BLOCK_SIZE - INDEX_SIZE
 
 # Connect and bind
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -18,20 +23,51 @@ server_socket.bind((host, port))
 # Address here is a pair "(host, port), which is used for the AF_INET address family"
 address = (host,port)
 
-# Get the data from buffer
-data,address = server_socket.recvfrom(buf) # Tuple (name, address)
+# Get the filename from buffer
+data,address = server_socket.recvfrom(BLOCK_SIZE) # Tuple (name, address)
 filename = data.strip()
 print "Receive file start:",filename
 
 # Open a file with the name of the transmitted file to write
 f = open(filename, 'wb') # 'wb' is for writing binary files
 
+# index calculations - this is tacked to the front of data
+size = os.path.getsize(filename)
+numBlocks = size / BLOCK_SIZE
+if size % BLOCK_SIZE != 0:
+    numBlocks += 1
+
+# instead of sending 512, we send 408 so 4 bytes will be the index #
+# we start at -1 because the first block of the file has the file name in it
+index = -1
+
 # Get the rest of the files
 try:
     while(data):
+        index += 1
+
+        print "The index is:", index
+
+        # Get the current index from the first few characters in data block
+        current_index = data[0:INDEX_SIZE]
+        if (index > 0):
+            print "The data block index is:", int(current_index)
+
+            # if the current index does not match the one transmitted, then
+            # alert the TCP helper!
+            if(index != int(current_index)):
+               print "uh oh! we got a corruption :D"
+               print "enter ANGELA"
+
+        # remove the filename from the header
+        data = data.replace(filename, "")
+
+        # replace the header of the blocks.
+        data = data.replace(current_index,"")
+
         f.write(data)
         server_socket.settimeout(2) # Round-trip time
-        data,address = server_socket.recvfrom(buf)
+        data,address = server_socket.recvfrom(BLOCK_SIZE)
 except socket.timeout:
     f.close()
     server_socket.close()
